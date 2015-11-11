306174a8bc0c4ae156013b38d97485671cbea9f3
porterhaus <bporter@porterhaus.com>
2015-11-11 12:58:23 -0600

    Setup routing framework, stateChange listeners, and AppController shell.

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,35 @@
-angular.module('ionicMeterApp', ['ionic'])
+(function(){
 
-.run(function($ionicPlatform) {
+  var app = angular.module('ionicMeterApp', ['ionic']);
 
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  app.run(function($ionicPlatform) {
+
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+
+    // Initialize Parse API with app keys.
+    Parse.initialize(
+      '2xSAk2c6Jzj9TzJcNcUKE42p7cKx6tNjiOKtLLrp',
+      'jHNUhXWZwoMwCqPH1aUpqau4oTbnwSuuIsesh5Ty'
+    );
+
   });
 
-  Parse.initialize('2xSAk2c6Jzj9TzJcNcUKE42p7cKx6tNjiOKtLLrp','jHNUhXWZwoMwCqPH1aUpqau4oTbnwSuuIsesh5Ty');
+  app.config(function($stateProvider, $urlRouterProvider){
 
-  var TestObject = Parse.Object.extend("TestObject");
-  var testObject = new TestObject();
-  testObject.save({foo: "bar"}).then(function(object) {
-    alert("yay! it worked");
+    $urlRouterProvider.otherwise('/');
   });
 
-});
+  app.run(function($rootScope){
+    $rootScope.$on('$stateChangeStart', function(next, fromParams, toParams, fromState, toState) {
+
+    });
+  });
+
+}());
---
+++ www/js/controllers.js
@@ -0,0 +1,9 @@
+(function(){
+
+  var app = angular.module('ionicMeterApp', ['ionic']);
+
+  app.controller('AppController', function() {
+    console.log('App Controller');
+  });
+
+}());
